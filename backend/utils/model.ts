8c67c0045294d2de3a2fee5b9c7a2c8f799446eb
Veaceslav BEZUŞCO <veaceslav.bezusco@orange.com>
2026-06-02 21:10:52 +0300

    fix(server): remove model summaraize node

--- backend/utils/model.ts
+++ backend/utils/model.ts
@@ -16,7 +16,7 @@ interface GetModelOptions {
 export async function getModel(options?: GetModelOptions) {
   const p = options?.provider ?? env.DEFAULT_PROVIDER;
   const customModel = options?.model;
-  const temperature = options?.temperature ?? 0;
+  const temperature = options?.temperature ?? 1;
 
   switch (p) {
     case "openai":
